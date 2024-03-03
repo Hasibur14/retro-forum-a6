e883a575cbf112aeae1d7724be2a4e5b6750caec
hasibur14 <hasib141415@gmail.com>
2024-03-03 17:23:59 +0600

    added news read count

--- app.js
+++ app.js
@@ -2,64 +2,77 @@ const loadAllNews = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts');
     const data = await res.json();
     const allData = data.posts;
-    //    console.log(db)
 
-
-    const newsContainer = document.getElementById('allNews-container')
+    const newsContainer = document.getElementById('allNews-container');
 
     allData.forEach((item) => {
-        console.log(item);
         const div = document.createElement('div');
-        div.classList = `card card-side bg-base-100 shadow-xl border-2 w-11/12`
+        div.classList = `card card-side bg-base-100 shadow-xl border-2 w-11/12`;
         div.innerHTML = `
-    <figure><img class="p-4 rounded-xl w-72"
-            src="${item.image
-            }"
-            alt="Movie" />
-    </figure>
-    <div class="card-body">
-        <div class="space-y-5 text-xl">
-            <div class="flex">
-                <p>${item.category}</p>
-                <p>${item.author.name}</p>
-            </div>
-            <div class="space-y-4">
-                <h4 class="lg:text-2xl font-bold">${item.title}</h4>
-                <p>${item.description}</p>
-            </div>
-            <hr>
-
-            <div class="flex justify-between ">
-                <div class="flex space-x-10">
-                    <div class="flex space-x-3">
-                        <span><img src="./images/tabler-icon-message-2.jpg" alt=""></span>
-                        <p>${item.comment_count
-                        }</p>
+            <figure>
+                <img class="p-4 rounded-xl w-72" src="${item.image}" alt="Movie" />
+            </figure>
+            <div class="card-body">
+                <div class="space-y-5 text-xl">
+                    <div class="flex">
+                        <p># ${item.category}</p>
+                        <p>Author: ${item.author.name}</p>
                     </div>
-                    <div class="flex space-x-3">
-                        <span><img src="./images/tabler-icon-eye.jpg" alt=""></span>
-                        <p>${item.view_count}</p>
+                    <div class="space-y-4">
+                        <h4 class="lg:text-2xl font-bold">${item.title}</h4>
+                        <p>${item.description}</p>
                     </div>
-                    <div class="flex space-x-3">
-                        <span><img src="./images/tabler-icon-clock-hour-9.jpg" alt=""></span>
-                        <p><span>${item.posted_time
-                        }</span> min</p>
+                    <hr>
+                    <div class="flex justify-between ">
+                        <div class="flex space-x-10">
+                            <div class="flex space-x-3">
+                                <span><img src="./images/tabler-icon-message-2.jpg" alt=""></span>
+                                <p>${item.comment_count}</p>
+                            </div>
+                            <div class="flex space-x-3">
+                                <span><img src="./images/tabler-icon-eye.jpg" alt=""></span>
+                                <p class="view">${item.view_count}</p>
+                            </div>
+                            <div class="flex space-x-3">
+                                <span><img src="./images/tabler-icon-clock-hour-9.jpg" alt=""></span>
+                                <p><span>${item.posted_time}</span> min</p>
+                            </div>
+                        </div>
+                        <div class="card-actions justify-end">
+                            <img class="email-img" data-title="${item.title}" data-view="${item.view_count}" src="./images/email 1.jpg" alt="">
+                        </div>
                     </div>
                 </div>
-                <div class="card-actions justify-end">
-                    <img src="./images/email 1.jpg" alt="">
-                </div>
             </div>
-        </div>
-    </div>
-
-    `;
-
+        `;
         newsContainer.appendChild(div);
-
     });
 
+    //  click event listeners to the email img:
+    const emailImages = document.querySelectorAll('.email-img');
+    emailImages.forEach((img) => {
+        img.addEventListener('click', handleEmailClick);
+    });
 };
 
+const handleEmailClick = (event) => {
+    const title = event.target.dataset.title;
+    const viewCount = event.target.dataset.view;
+    countContainer(title, viewCount);
+};
 
-loadAllNews()
+const countContainer = (title, viewCount) => {
+    const countCard = document.getElementById('card-count');
+    const div = document.createElement('div');
+    div.classList = `flex bg-white p-4 justify-between rounded-lg`;
+    div.innerHTML = `    
+       <h4>${title}</h4>
+       <div class="flex">
+           <img src="./images/tabler-icon-eye.jpg" alt="">
+           <p>${viewCount}</p>
+       </div>
+    `;
+    countCard.appendChild(div);
+};
+
+loadAllNews();
